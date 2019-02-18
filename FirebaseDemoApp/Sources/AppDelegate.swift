35a7816ce2af58b2a1c364e96a76f68900ec954e
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2019-02-18 10:20:03 +0900

    Base Project

---
+++ FirebaseDemoApp/Sources/AppDelegate.swift
@@ -0,0 +1,16 @@
+// ***********************************************************************
+// Firebase Demo App
+//
+// Copyright (C) Yuichi NAKAYASU All rights reserved.
+// ***********************************************************************
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
